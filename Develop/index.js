71b78dfb0d5e807d34fb01fb02c925dd2155b291
KvTsira <kvtsira@gmail.com>
2022-01-30 22:15:51 -0500

    initiatl inquirer setup

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,17 @@
 // TODO: Include packages needed for this application
-const inquirer = require ('inquire');
+const inquirer = require ('inquirer');
+console.log(inquirer)
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
 
 
